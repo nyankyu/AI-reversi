cefac250a3e1cf2dd09cf007bd31222322e82672
nyankyu <fukujinn@gmail.com>
2021-12-24 00:32:57 +0900

    Fix out of range reference.

--- Google_tests/BoardTest.cpp
+++ Google_tests/BoardTest.cpp
@@ -20,4 +20,42 @@ protected:
 
 TEST_F(BoardTest, Constructor) {
   ASSERT_TRUE(board != nullptr);
+  ASSERT_TRUE(board->print != nullptr);
+}
+
+TEST_F(BoardTest, Initialize) {
+  Board_init(board);
+  ASSERT_EQ(BOARD_SIZE * BOARD_SIZE - 4, board->empty);
+  ASSERT_EQ(2, board->white);
+  ASSERT_EQ(2, board->black);
+
+  int W = WALL;
+  int E = EMPTY;
+  int O = WHITE;
+  int X = BLACK;
+  int box[] = {
+      W,W,W,W,W,W,W,W,W,
+      W,E,E,E,E,E,E,E,E,
+      W,E,E,E,E,E,E,E,E,
+      W,E,E,E,E,E,E,E,E,
+      W,E,E,E,O,X,E,E,E,
+      W,E,E,E,X,O,E,E,E,
+      W,E,E,E,E,E,E,E,E,
+      W,E,E,E,E,E,E,E,E,
+      W,E,E,E,E,E,E,E,E,
+      W,W,W,W,W,W,W,W,W,W
+  };
+  for (int i = 0; i < BOX_SIZE; i++)
+    ASSERT_EQ(box[i], board->box[i]);
+}
+
+TEST_F(BoardTest, Copy) {
+  board->box[0] = 10;
+  board->box[BOARD_SIZE + 1] = 11;
+  board->box[10] = 12;
+  board->box[20] = 13;
+  board->box[BOX_SIZE - 1] = 14;
+  Board *copy = Board_copy(board);
+  for (int i = 0; i < BOX_SIZE; i++)
+    ASSERT_EQ(board->box[i], copy->box[i]);
 }
