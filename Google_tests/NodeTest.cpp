dd68af9fdf82a0d7d0fc30dbdd7620396aa342fc
nyankyu <fukujinn@gmail.com>
2021-12-24 00:33:37 +0900

    Add NodeTest.

---
+++ Google_tests/NodeTest.cpp
@@ -0,0 +1,15 @@
+#include "gtest/gtest.h"
+
+extern "C" {
+#include "node.h"
+}
+
+class NodeTest : public ::testing::Test {
+protected:
+  virtual void SetUp() {
+
+  }
+  virtual void TearDown() {
+
+  }
+};
