3698e352c1fda364082b154ffe6d22841e2a0e8e
nyankyu <fukujinn@gmail.com>
2022-01-09 03:48:27 +0900

    Add rule-base evaluator.

--- include/board.h
+++ include/board.h
@@ -18,6 +18,78 @@
 #define DIR_DOWN        9
 #define DIR_DOWN_RIGHT  10
 
+#define A1  10
+#define A2  11
+#define A3  12
+#define A4  13
+#define A5  14
+#define A6  15
+#define A7  16
+#define A8  17
+
+#define B1  19
+#define B2  20
+#define B3  21
+#define B4  22
+#define B5  23
+#define B6  24
+#define B7  25
+#define B8  26
+
+#define C1  28
+#define C2  29
+#define C3  30
+#define C4  31
+#define C5  32
+#define C6  33
+#define C7  34
+#define C8  35
+
+#define D1  37
+#define D2  38
+#define D3  39
+#define D4  40
+#define D5  41
+#define D6  42
+#define D7  43
+#define D8  44
+
+#define E1  46
+#define E2  47
+#define E3  48
+#define E4  49
+#define E5  50
+#define E6  51
+#define E7  52
+#define E8  53
+
+#define F1  55
+#define F2  56
+#define F3  57
+#define F4  58
+#define F5  59
+#define F6  60
+#define F7  61
+#define F8  62
+
+#define G1  64
+#define G2  65
+#define G3  66
+#define G4  67
+#define G5  68
+#define G6  69
+#define G7  70
+#define G8  71
+
+#define H1  73
+#define H2  74
+#define H3  75
+#define H4  76
+#define H5  77
+#define H6  78
+#define H7  79
+#define H8  80
+
 #define POS(x,y) ((BOARD_SIZE + 1) * y + x)
 
 typedef struct board Board;
--- src/evaluator.c
+++ src/evaluator.c
@@ -1,7 +1,111 @@
+#include <limits.h>
 #include "evaluator.h"
+#include "rule.h"
+#include "ai-reversi.h"
+
+int available_point(Node *node) {
+  int count = 0;
+
+  for (int y = 1; y <= BOARD_SIZE; y++) {
+    for (int x = 1; x <= BOARD_SIZE; x++) {
+      if (Rule_can_set(node->board, x, y, WHITE) == OK) {
+        if (node->next_color == WHITE)
+          count++;
+        else
+          count--;
+      }
+    }
+  }
+  return count;
+}
+
+int important_point(Node *node) {
+  int count = 0;
+
+  if (node->board->box[A1] == WHITE)
+    count += 60;
+  if (node->board->box[A1] == BLACK)
+    count -= 60;
+  if (node->board->box[H1] == WHITE)
+    count += 60;
+  if (node->board->box[H1] == BLACK)
+    count -= 60;
+  if (node->board->box[A8] == WHITE)
+    count += 60;
+  if (node->board->box[A8] == BLACK)
+    count -= 60;
+  if (node->board->box[H8] == WHITE)
+    count += 60;
+  if (node->board->box[H8] == BLACK)
+    count -= 60;
+
+  if (node->board->box[B1] == WHITE)
+    count += 30;
+  if (node->board->box[B1] == BLACK)
+    count -= 30;
+  if (node->board->box[G1] == WHITE)
+    count += 30;
+  if (node->board->box[G1] == BLACK)
+    count -= 30;
+  if (node->board->box[A2] == WHITE)
+    count += 30;
+  if (node->board->box[A2] == BLACK)
+    count -= 30;
+  if (node->board->box[H2] == WHITE)
+    count += 30;
+  if (node->board->box[H2] == BLACK)
+    count -= 30;
+  if (node->board->box[A7] == WHITE)
+    count += 30;
+  if (node->board->box[A7] == BLACK)
+    count -= 30;
+  if (node->board->box[H7] == WHITE)
+    count += 30;
+  if (node->board->box[H7] == BLACK)
+    count -= 30;
+  if (node->board->box[B8] == WHITE)
+    count += 30;
+  if (node->board->box[B8] == BLACK)
+    count -= 30;
+  if (node->board->box[G8] == WHITE)
+    count += 30;
+  if (node->board->box[G8] == BLACK)
+    count -= 30;
+
+  if (node->board->box[B2] == WHITE)
+    count += 60;
+  if (node->board->box[B2] == BLACK)
+    count -= 60;
+  if (node->board->box[G2] == WHITE)
+    count += 60;
+  if (node->board->box[G2] == BLACK)
+    count -= 60;
+  if (node->board->box[B7] == WHITE)
+    count += 60;
+  if (node->board->box[B7] == BLACK)
+    count -= 60;
+  if (node->board->box[G7] == WHITE)
+    count += 60;
+  if (node->board->box[G7] == BLACK)
+    count -= 60;
+
+  return count;
+}
 
 void Evaluator_evaluate(Node *node, int my_color) {
-  node->eval_point = (my_color == WHITE) ? node->board->black : node->board->white;
+  int available = available_point(node);
+
+  if (available == 0) {
+    node->eval_point = (node->board->white > node->board->black) ? INT_MAX : -INT_MAX;
+    if (my_color == BLACK)
+      node->eval_point *= -1;
+    return;
+  }
+
+  int important = important_point(node);
+  node->eval_point = available + important;
+  if (node->next_color == BLACK)
+    node->eval_point *= -1;
   if (my_color != node->next_color)
     node->eval_point *= -1;
 }
