f223cbef371a4a940383be6900cf47a9b1ea3bc5
nyankyu <fukujinn@gmail.com>
2022-01-13 23:57:34 +0900

    Add game mode.
    Add Human vs AI, Human vs Human mode. Shitching the mode is done with the launch argument.

--- src/main.c
+++ src/main.c
@@ -12,33 +12,54 @@ static void print_prompt(int next_color) {
   printf("\n %c > ", next_color == WHITE ? 'O' : 'X');
 }
 
+static void usage(void) {
+  printf("usage: ai-reversi [option]\n");
+  printf("option\n");
+  printf("\t(none)      The same as \"black\"\n");
+  printf("\tauto        AI vs AI\n");
+  printf("\twhite       Human vs AI. Human is White. AI is Black.\n");
+  printf("\tblack       Human vs AI. Human is Black. AI is White.\n");
+  printf("\tvs_human    Human vs Human\n");
+}
 
 int main(int argc, char **argv) {
-  int ai_vs_ai = FALSE;
-  if (argc == 2 && strcmp("auto", argv[1]) == 0) {
-    ai_vs_ai = TRUE;
-  }
-
   Player *black;
   Player *white;
   Player *player;
   Board *board = Board_new();
   History *history = History_new();
-  int next_color = BLACK;
   int pass = 0;
 
-  if (ai_vs_ai == TRUE)
-    black = Com_make_player(BLACK);
-  else
+  if (argc == 1) {
+    white = Com_make_player(WHITE);
     black = Human_make_player(BLACK);
-  white = Com_make_player(WHITE);
+  } else if (argc != 2) {
+    usage();
+    exit(EXIT_ERR);
+  } else if (strcmp("auto", argv[1]) == 0) {
+    white = Com_make_player(WHITE);
+    black = Com_make_player(BLACK);
+  } else if (strcmp("white", argv[1]) == 0) {
+    white = Human_make_player(WHITE);
+    black = Com_make_player(BLACK);
+  } else if (strcmp("black", argv[1]) == 0) {
+    white = Com_make_player(WHITE);
+    black = Human_make_player(BLACK);
+  } else if (strcmp("vs_human", argv[1]) == 0) {
+    white = Human_make_player(WHITE);
+    black = Human_make_player(BLACK);
+  } else {
+    usage();
+    exit(EXIT_ERR);
+  }
+
   player = black;
   Board_init(board);
   History_add(history, board, BLACK);
 
   while (1) {
     Board_print(board);
-    print_prompt(next_color);
+    print_prompt(player->color);
 
     int result = player->next(player, board);
     if (result == PLAYER_PUT) {
@@ -53,15 +74,16 @@ int main(int argc, char **argv) {
       History_undo(history);
       History_undo(history);
       Board_rewrite(board, history->board_array[history->current]);
+      if (history->current == 0)
+        player = black;
       continue;
     } else if (result == PLAYER_ERR){
       exit(EXIT_ERR);
     } else if (result == PLAYER_NG) {
       continue;
     }
-    History_add(history, board, next_color);
+    History_add(history, board, player->color);
     player = (player == white) ? black : white;
-    next_color = Rule_other_color(next_color);
   }
 
   History_delete(history);
